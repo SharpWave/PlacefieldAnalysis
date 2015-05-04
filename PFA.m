709df3ff4c87a44a63e1ea7ef65fde4cc8898409
SharpWave <sullivan.dw@gmail.com>
2015-05-04 16:35:17 -0400

    bugfix

--- PFA.m
+++ PFA.m
@@ -1,6 +1,8 @@
-function [] = PFA(roomstr)
+function [] = PFA(roomstr,rawfile)
 % [] = PFA(roomstr)
-%   Detailed explanation goes here
+% roomstr is the name of the room e.g. '201a'
+% rawfile is the same of the .h5 file containing the original movie
+
 
 % Step 1: Calculate Placefields
 CalculatePlacefields(roomstr);
@@ -9,7 +11,7 @@ CalculatePlacefields(roomstr);
 PFstats();
 
 % Step 3: Extract Traces
-ExtractTracesProc('D1Movie.h5',infile)
+ExtractTracesProc('D1Movie.h5',rawfile)
 
 % Step 4: Browse placefields
 PFbrowse('D1Movie.h5');
